cee183e0d547be208e98efce3124c3e38c028c2c
Patrik Hedman <patrik.hedman@gmail.com>
2014-07-28 13:16:57 +0200

    remove unnecessary comment

--- src/lib/control/functor/index.js
+++ src/lib/control/functor/index.js
@@ -2,11 +2,7 @@
     'use strict';
     var Protocol$2511 = require('../../core').Protocol;
     var unary$2513 = require('../../data/function').unary;
-    var
-        // ***************************************************************
-        // **                  CurryJS.Control.Functor                  **
-        // ***************************************************************
-        Functor$2515 = Protocol$2511('Functor', { prototype: { map: Protocol$2511.required } });
+    var Functor$2515 = Protocol$2511('Functor', { prototype: { map: Protocol$2511.required } });
     // map :: Functor f => (a -> b) -> f a -> f b
     function map$2517(f$2518, xs$2519) {
         return function map$2517(f$2520, xs$2521) {
